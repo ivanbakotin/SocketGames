6b7b2bf80720ecb293a5f108dffc4184ab0ffe41
ivan <ivan.bakotin1999@gmail.com>
2022-02-08 15:44:09 +0100

    setup and menu

---
+++ script.js
@@ -0,0 +1,11 @@
+const game = document.querySelector(".game");
+const menu = document.querySelector(".menu");
+const leaderboard = document.querySelector(".leaderboard");
+const leaderboardButton = document.querySelector(".leaderboard-button");
+const playButton = document.querySelector(".play-button");
+
+playButton.addEventListener("click", displayGame);
+
+function displayGame() {
+    console.log("game")
+}
